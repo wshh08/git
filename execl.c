8ad32558375c38234a813412a60c52ef0d19adc6
wangshaohua <wshh08@126.com>
2014-12-01 19:33:06 +0800

    增加注释2

--- execl.c
+++ execl.c
@@ -5,6 +5,6 @@ main()
     printf("Excuting ls\n");
     execl("/bin/ls","ls","-l",NULL); //通过execl执行ls。
 
-    perror("execl failed to run ls");
+    perror("execl failed to run ls"); //错误处理。
     exit(1);
 }
